6c35ea1bc580eccd5d8b96c41b46d8128c180bbf
aizirek toktosunova <ajzirektoktosunova853@gmail.com>
2026-07-11 16:35:02 +0600

    Исправлена настройка отправки почты через Gmail SMTP 587

--- src/main/java/com/ulutman/config/EmailConfig.java
+++ src/main/java/com/ulutman/config/EmailConfig.java
@@ -1,39 +1,3 @@
-//package com.ulutman.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.JavaMailSenderImpl;
-//
-//import java.util.Properties;
-//
-//@Slf4j
-//@Configuration
-//@RequiredArgsConstructor
-//public class EmailConfig {
-//
-//    @Bean
-//    public JavaMailSender javaMailSender() {
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//
-//        log.info("SMTP host: smtp.gmail.com");
-//        log.info("SMTP port: 587");
-//        mailSender.setHost("smtp.gmail.com");
-//        mailSender.setPort(587);
-//        mailSender.setUsername("ulutmanproject@gmail.com");
-//        mailSender.setPassword("wraj wuqm upmk sjbc");
-//        Properties props = mailSender.getJavaMailProperties();
-//        props.put("mail.transport.protocol", "smtp");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.debug", "true");
-//        props.put("mail.smtp.encoding", "UTF-8");
-//
-//        return mailSender;
-//    }
-//}
 package com.ulutman.config;
 
 import lombok.RequiredArgsConstructor;
@@ -45,31 +9,30 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
-@RequiredArgsConstructor
-@Configuration
 @Slf4j
+@Configuration
+@RequiredArgsConstructor
 public class EmailConfig {
 
     @Bean
     public JavaMailSender javaMailSender() {
-
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        System.out.println("МОЙ EMAIL CONFIG ЗАПУЩЕН");
 
+        log.info("SMTP host: smtp.gmail.com");
+        log.info("SMTP port: 587");
         mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(465);
-
+        mailSender.setPort(587);
         mailSender.setUsername("ulutmanproject@gmail.com");
         mailSender.setPassword("wraj wuqm upmk sjbc");
         Properties props = mailSender.getJavaMailProperties();
-
-        props.put("mail.smtp.from", "ulutmanproject@gmail.com");
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
         props.put("mail.debug", "true");
         props.put("mail.smtp.encoding", "UTF-8");
 
         return mailSender;
     }
 }
+
